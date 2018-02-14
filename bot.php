44afe03fb69ba8c3dc63cad4ff5d54daa6e0385a
bbbbonus <36473040+bbbbonus@users.noreply.github.com>
2018-02-14 23:51:41 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -9,12 +9,35 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
-		if($event['type'] == 'follow'){
+		if($event['type'] == 'follow' && $event['source']['type'] == 'user'){
 			$userid = $event['source']['userId'];
-			$file = 'test.txt';
-			$current = file_get_contents($file);
-			$current .= $userid;
-			file_put_contents($file, $current);
+			$replyToken = $event['replyToken'];
+			$str = 'Welcome to EON Solution';
+			
+			$messages = [
+				'type' => 'text',
+				'text' => $str
+			];
+			
+			$url = 'https://api.line.me/v2/bot/message/push';
+			$data = [
+				'to' => $userid,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+			
 		}
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
@@ -35,7 +58,7 @@ if (!is_null($events['events'])) {
 			}else{
 				$messages = [
 				'type' => 'text',
-				'text' => $userid
+				'text' => $text
 			];
 			}
 
